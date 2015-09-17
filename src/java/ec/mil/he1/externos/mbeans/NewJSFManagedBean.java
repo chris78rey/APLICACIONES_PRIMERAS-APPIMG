6ff0c97e189f9d79ab4c462d22e240be0d00220f
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-09-17 06:36:10 -0500

    ya funco el filtro

---
+++ src/java/ec/mil/he1/externos/mbeans/NewJSFManagedBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.externos.mbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import javax.inject.Named;
+
+@Named
+/**
+ *
+ * @author christian_ruiz
+ */
+@ManagedBean
+@ViewScoped
+public class NewJSFManagedBean {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+    
+}
